f0bce75b54911f41574b53ace61f06fbbc5dc09e
Igor Damasceno <igor.damasceno@codeminer42.com>
2023-03-08 18:40:46 -0300

    refactor: add task notification decorators to complete task service

--- app/services/complete_task.rb
+++ app/services/complete_task.rb
@@ -1,6 +1,4 @@
 class CompleteTask < ApplicationService
-  attr_reader :task
-
   def initialize(task:)
     @task = task
   end
@@ -8,44 +6,25 @@ class CompleteTask < ApplicationService
   def call
     task.update!(status: :completed, completed_at: Time.current)
 
-    if task.user.notification_preferences['task_completed'].present?
-      if task.user.notification_preferences['task_completed'].include?('email')
-        TaskMailer.with(task:).task_completed_notification.deliver_now
-      end
-
-      if task.user.notification_preferences['task_completed'].include?('sms')
-        sms_client = FakeSmsClient.new(
-          account_id: ENV.fetch('FAKE_SMS_ACCOUNT_ID'),
-          auth_token: ENV.fetch('FAKE_SMS_AUTH_TOKEN')
-        )
-        sms_client.send_sms(
-          from: ENV.fetch('FAKE_SMS_PHONE_NUMBER'),
-          to: task.user.phone_number,
-          body: "Task \"#{task.name}\" completed!"
-        )
-      end
-
-      if task.user.notification_preferences['task_completed'].include?('telegram')
-        telegram_client = FakeTelegramClient.new(auth_token: ENV.fetch('FAKE_TELEGRAM_AUTH_TOKEN'))
-        telegram_client.send_message(
-          chat_id: task.user.telegram_chat_id,
-          text: "Task \"#{task.name}\" completed!"
-        )
-      end
-
-      if task.user.notification_preferences['task_completed'].include?('whatsapp')
-        whatsapp_client = FakeWhatsappClient.new(
-          account_id: ENV.fetch('FAKE_WHATSAPP_ACCOUNT_ID'),
-          auth_token: ENV.fetch('FAKE_WHATSAPP_AUTH_TOKEN')
-        )
-        whatsapp_client.send_message(
-          from: "whatsapp:#{ENV.fetch('FAKE_WHATSAPP_PHONE_NUMBER')}",
-          to: "whatsapp:#{task.user.phone_number}",
-          body: "Task \"#{task.name}\" completed!"
-        )
-      end
-    end
+    notify_user if task.user.notification_preferences['task_completed'].present?
 
     task
+  end
+
+  private
+
+  attr_reader :task
+
+  NOTIFICATION_DECORATORS = {
+    'email' => TaskEmailNotificationDecorator,
+    'sms' => TaskSmsNotificationDecorator,
+    'telegram' => TaskTelegramNotificationDecorator,
+    'whatsapp' => TaskWhatsappNotificationDecorator
+  }.freeze
+
+  private_constant :NOTIFICATION_DECORATORS
+
+  def notify_user
+    task.user.notification_preferences['task_completed'].each { |channel| NOTIFICATION_DECORATORS[channel].new(task:) }
   end
 end
